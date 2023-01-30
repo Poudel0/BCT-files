3f1b1bceef24228b9697fbfa572b32581093a6de
Poudel0 <poudel0sandesh@gmail.com>
2023-01-30 22:43:14 +0545

    DSA

---
+++ insertion.cpp
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+
+int main()
+{
+	int arr[100];
+	int i, x,n, pos, num;
+	printf("Enter the number of elements");
+	scanf("%d",&n);
+
+	printf("Enter the elements");
+	for (i = 0; i < n; i++){
+	
+	
+	scanf("%d", &arr[i]);
+	}
+
+	for (i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+
+
+printf("Enter the value to update");
+scanf("%d",&x);
+
+printf("Enter the position to update");
+scanf("%d",&pos);
+
+
+
+
+	n++;
+
+
+	for (i = n - 1; i >= pos; i--)
+		arr[i] = arr[i - 1];
+
+
+	arr[pos - 1] = x;
+
+
+	for (i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+
+	return 0;
+}
+
